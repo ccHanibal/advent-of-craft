c56a2cb3930a0099a8df6c87820f563f2cc66de7
Oliver Daum <oliver.daum@codecentric.de>
2023-12-02 13:20:48 +0100

    Using guard clauses for day 2

--- exercise/day02/src/main/java/games/FizzBuzz.java
+++ exercise/day02/src/main/java/games/FizzBuzz.java
@@ -4,24 +4,22 @@ public class FizzBuzz {
     private FizzBuzz() {
     }
     
-    public static String convert(Integer input) throws OutOfRangeException {
-        if (input > 0) {
-            if (input <= 100) {
-                if (input % 3 == 0 && input % 5 == 0) {
-                    return "FizzBuzz";
-                }
-                if (input % 3 == 0) {
-                    return "Fizz";
-                }
-                if (input % 5 == 0) {
-                    return "Buzz";
-                }
-                return input.toString();
-            } else {
-                throw new OutOfRangeException();
-            }
-        } else {
+    public static String convert(int input) throws OutOfRangeException {
+        if (input <= 0)
             throw new OutOfRangeException();
-        }
+
+        if (input > 100)
+            throw new OutOfRangeException();
+
+        if (input % 3 == 0 && input % 5 == 0)
+            return "FizzBuzz";
+
+        if (input % 3 == 0)
+            return "Fizz";
+
+        if (input % 5 == 0)
+            return "Buzz";
+        
+        return input.toString();
     }
 }
