dcfcda27270fb6cb6d0c7286981e9c83aec1bb99
meHanibal <oliver91daum@hotmail.de>
2023-12-09 14:41:48 +0100

    Made Client class stateless

--- exercise/day09/src/main/java/account/Client.java
+++ exercise/day09/src/main/java/account/Client.java
@@ -5,26 +5,50 @@ import java.util.stream.Collectors;
 
 public class Client {
     private final Map<String, Double> orderLines;
-    private double totalAmount;
 
     public Client(Map<String, Double> orderLines) {
         this.orderLines = orderLines;
     }
 
-    public String toStatement() {
+    /**
+     * Generates a summary of all orders, their prices and the total amount.
+     */
+    @Deprecated
+    public String generateSummary() {
         return orderLines.entrySet().stream()
                 .map(entry -> formatLine(entry.getKey(), entry.getValue()))
                 .collect(Collectors.joining(System.lineSeparator()))
-                .concat(System.lineSeparator() + "Total : " + totalAmount + "€");
+                .concat(System.lineSeparator() + "Total : " + getTotalAmount() + "€");
+    }
+    /**
+     ** Memory optimized version of {@link #generateSummary()} by summing
+     * the total amount on the way, therefore iterating the orders only once.
+     **/
+    public String generateSummaryFast() {
+        double totalAmount = 0.0;
+        StringBuilder result = new StringBuilder();
+
+        for (Map.Entry<String, Double> entry : orderLines.entrySet()) {
+            totalAmount += entry.getValue();
+            result.append(formatLine(entry.getKey(), entry.getValue()))
+                    .append(System.lineSeparator());
+        }
+
+        return result.append("Total : ").append(totalAmount).append("€")
+                .toString();
+    }
+
+    /**
+     * Sums up all prices of the order.
+     */
+    public double getTotalAmount() {
+        return orderLines.values().stream()
+                .mapToDouble(v -> v)
+                .sum();
     }
 
     private String formatLine(String name, Double value) {
-        totalAmount += value;
         return name + " for " + value + "€";
-    }
-
-    public double getTotalAmount() {
-        return totalAmount;
     }
 }
 
--- exercise/day09/src/test/java/ClientTests.java
+++ exercise/day09/src/test/java/ClientTests.java
@@ -1,4 +1,5 @@
 import account.Client;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedHashMap;
@@ -7,23 +8,68 @@ import static java.lang.System.lineSeparator;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ClientTests {
-    private final Client client = new Client(new LinkedHashMap<>() {{
-        put("Tenet Deluxe Edition", 45.99);
-        put("Inception", 30.50);
-        put("The Dark Knight", 30.50);
-        put("Interstellar", 23.98);
-    }});
+    private Client client;
+
+    @BeforeEach
+    void setupTest() {
+        client = new Client(new LinkedHashMap<>() {{
+            put("Tenet Deluxe Edition", 45.99);
+            put("Inception", 30.50);
+            put("The Dark Knight", 30.50);
+            put("Interstellar", 23.98);
+        }});
+    }
 
     @Test
-    void client_should_return_statement() {
-        String statement = client.toStatement();
+    void client_should_generate_summary() {
+        String summary = client.generateSummary();
 
-        assertThat(client.getTotalAmount()).isEqualTo(130.97);
-        assertThat(statement).isEqualTo(
+        assertThat(summary).isEqualTo(
                 "Tenet Deluxe Edition for 45.99€" + lineSeparator() +
                         "Inception for 30.5€" + lineSeparator() +
                         "The Dark Knight for 30.5€" + lineSeparator() +
                         "Interstellar for 23.98€" + lineSeparator() +
                         "Total : 130.97€");
+    }
+
+    @Test
+    void client_does_generate_same_summary_twice_in_a_row() {
+        String summary1 = client.generateSummary();
+        String summary2 = client.generateSummary();
+
+        assertThat(summary1).isEqualTo(summary2);
+    }
+
+    @Test
+    void client_can_give_total_amount_without_generating_summary_first() {
+        assertThat(client.getTotalAmount()).isEqualTo(130.97);
+    }
+
+    @Test
+    void client_should_generate_summary_fast() {
+        String summary = client.generateSummaryFast();
+
+        assertThat(summary).isEqualTo(
+                "Tenet Deluxe Edition for 45.99€" + lineSeparator() +
+                        "Inception for 30.5€" + lineSeparator() +
+                        "The Dark Knight for 30.5€" + lineSeparator() +
+                        "Interstellar for 23.98€" + lineSeparator() +
+                        "Total : 130.97€");
+    }
+
+    @Test
+    void client_does_generate_same_summary_twice_in_a_row_fast() {
+        String summary1 = client.generateSummaryFast();
+        String summary2 = client.generateSummaryFast();
+
+        assertThat(summary1).isEqualTo(summary2);
+    }
+
+    @Test
+    void client_does_generate_same_summary_slow_vs_fast() {
+        String summary1 = client.generateSummary();
+        String summary2 = client.generateSummaryFast();
+
+        assertThat(summary1).isEqualTo(summary2);
     }
 }
